0afafce2bb463b4da52c2203f331a46076777368
陈扬帆 <cyf3211@qq.com>
2016-12-19 15:41:08 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,6 +13,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
@@ -43,7 +44,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
    end
 
    def destroy
-     
+
 
      @group.destroy
      redirect_to groups_path, alert: "Group deleted"
